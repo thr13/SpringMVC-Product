0bf8e088b72b2b369d864ab3ce2ff73d5f158a0c
sugwan <skhrnt2945@gmail.com>
2025-07-12 11:12:47 +0900

    feat: @ModelAttribute 사용법

--- src/main/java/hello/item_service/web/basic/BasicItemController.java
+++ src/main/java/hello/item_service/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,9 +36,64 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @GetMapping("/add")
-    public String save() {
-        return "basic/addForm"; // addForm 에서 Post 를 호출하면 실행
+    //    @PostMapping("/add")
+    public String addItemV1(@RequestParam("itemName") String itemName,
+                            @RequestParam("price") int price,
+                            @RequestParam("quantity") Integer quantity,
+                            Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item); // 저장된 결과물을 바로 삽입
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) { // Model 에 들어가는 값 "item,
+        /*
+        @ModelAttribute("item") 는 아래 코드와 같은 의미이다
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+         */
+
+        itemRepository.save(item);
+        /*
+        @ModelAttribute 는 Model 객체를 만들어 주고 자동으로 View 에 넣어준다 그러므로 아래 코드는 생략 가능
+        model.addAttribute("item", item);
+         */
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        /*
+        @ModelAttribute 파라미터 생략시, 뒤의 클래스명의 첫글자를 소문자로 변경하고 ModelAttribute 에 담기게 된다
+        Item -> item
+         */
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        /*
+        단순 타입인 경우 @ModelAttribute 생략시 @RequestParam 이 적용되고
+        객체인경우 @ModelAttribute 생략시 ModelAttribute 가 적용된다
+        그러므로 Item -> item 로 변경되고 이게 ModelAttribute 로 담기게 된다
+         */
+        itemRepository.save(item);
+
+        return "basic/item";
     }
 
     /**
