7ca77204a6d925cc2d1eb3123be291c4121bc2a9
sugwan <skhrnt2945@gmail.com>
2025-07-12 11:38:04 +0900

    feat: PRG 로 POST 재요청 방지

--- src/main/java/hello/item_service/web/basic/BasicItemController.java
+++ src/main/java/hello/item_service/web/basic/BasicItemController.java
@@ -54,7 +54,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-//    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item) { // Model 에 들어가는 값 "item,
         /*
         @ModelAttribute("item") 는 아래 코드와 같은 의미이다
@@ -73,7 +73,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-//    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         /*
         @ModelAttribute 파라미터 생략시, 뒤의 클래스명의 첫글자를 소문자로 변경하고 ModelAttribute 에 담기게 된다
@@ -84,7 +84,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
         /*
         단순 타입인 경우 @ModelAttribute 생략시 @RequestParam 이 적용되고
@@ -94,6 +94,19 @@ public class BasicItemController {
         itemRepository.save(item);
 
         return "basic/item";
+        /*
+        서버 내부에서 POST 요청하고, HTTP 응답 데이터 만들어 줌 ->
+        새로고침시 계속 POST 전송하므로 문제 발생함 ->
+        그러므로 저장을 하고 나서 redirect 를 하면 된다
+        (즉, 상품 저장 후 ViewTemplate 로 이동하는게 아닌, 상품 상세 화면으로 redirect 를 호출해주면 된다(URL 자체가 바뀌므로 계속 요청(GET)해도 문제가 되지 않는다)
+         */
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId(); // RPG(Post/Redirect/Get) 적용
     }
 
     @GetMapping("/{itemId}/edit")
