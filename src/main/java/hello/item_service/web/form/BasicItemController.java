f1d87793d29eca71f03575383eef94ed0869763e
sugwan <skhrnt2945@gmail.com>
2025-07-14 23:38:05 +0900

    fix: thymeleaf - 선택 변수식 th:field=*{} 로 사용할 객체 지정

--- src/main/java/hello/item_service/web/form/BasicItemController.java
+++ src/main/java/hello/item_service/web/form/BasicItemController.java
@@ -32,7 +32,8 @@ public class BasicItemController {
     }
 
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
         return "form/addForm";
     }
 
