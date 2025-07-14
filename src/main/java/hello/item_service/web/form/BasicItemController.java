6a7cfc370c78e1575d8f1172697db8ac7adef02e
sugwan <skhrnt2945@gmail.com>
2025-07-15 00:04:32 +0900

    feat: 체크박스와 null hidden 타입과 name 에 _를 추가하여 미체크시 스프링 MVC 가 체크 해제를 판단하여 null 값 대신 false 반환

--- src/main/java/hello/item_service/web/form/BasicItemController.java
+++ src/main/java/hello/item_service/web/form/BasicItemController.java
@@ -3,6 +3,7 @@ package hello.item_service.web.form;
 import hello.item_service.domain.item.Item;
 import hello.item_service.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -40,6 +42,9 @@ public class BasicItemController {
 
     @PostMapping("/add")
     public String addItem(Item item, RedirectAttributes redirectAttributes) {
+
+        log.info("item.open={}", item.getOpen());
+
         Item saveItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", saveItem.getId());
         redirectAttributes.addAttribute("status", true);
