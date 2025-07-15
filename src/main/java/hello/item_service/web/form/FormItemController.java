60b5feae630a2282fc8e70a0a6532a6080038aa9
sugwan <skhrnt2945@gmail.com>
2025-07-15 22:32:28 +0900

    fix: thymeleaf - 멀티 체크박스

--- src/main/java/hello/item_service/web/form/FormItemController.java
+++ src/main/java/hello/item_service/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,16 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions") // 자동으로 model.addAttribute 메소드가 실행됨
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
 
     @GetMapping()
     public String items(Model model) {
@@ -42,8 +54,8 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(Item item, RedirectAttributes redirectAttributes) {
-
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
 
         Item saveItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", saveItem.getId());
