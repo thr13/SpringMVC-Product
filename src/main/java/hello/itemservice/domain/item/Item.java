581ab87d89d8803e99d3151d0ffcbd93a2879551
sugwan <skhrnt2945@gmail.com>
2025-07-19 20:55:43 +0900

    feat: validation 어노테이션

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,25 @@
 package hello.itemservice.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range; // range 는 표준이 아니므로, hibernate validator 를 구현한 구현체를 사용할때만 제공된다
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백이 들어가서는 안됩니다")
     private String itemName;
+
+    @NotNull
+    @Range(min=1000, max=1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
