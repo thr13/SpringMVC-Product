00e2188414891b7d7f163d5f56298681e229073c
sugwan <skhrnt2945@gmail.com>
2025-07-20 21:59:01 +0900

    feat: Bean Validation 에러 메시지

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,13 +5,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range; // range 는 표준이 아니므로, hibernate validator 를 구현한 구현체를 사용할때만 제공된다
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.") // 제약이 많고 복잡하므로 직접 자바 코드로 작성하는걸 권장함
 public class Item {
 
+    @NotNull // 수정 요구사항 추가.
     private Long id;
 
-    @NotBlank(message = "공백이 들어가서는 안됩니다")
+    @NotBlank
     private String itemName;
 
     @NotNull
@@ -19,7 +22,7 @@ public class Item {
     private Integer price;
 
     @NotNull
-    @Max(9999)
+    //@Max(9999) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
