35ce3d215c85a48e40e08fd33178cf733f3f4ca7
thr13 <skhrnt2945@gmail.com>
2025-07-22 19:01:18 +0900

    feat: spring-interceptor 설정 추가

--- src/main/java/hello/itemservice/web/WebConfig.java
+++ src/main/java/hello/itemservice/web/WebConfig.java
@@ -2,15 +2,26 @@ package hello.itemservice.web;
 
 import hello.itemservice.web.filter.LogFilter;
 import hello.itemservice.web.filter.LoginCheckFilter;
+import hello.itemservice.web.interceptor.LogInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**") // 하위 경로 모두 적용
+                .excludePathPatterns("/css/**", "/*.ico", "/error"); // 인터셉터를 제외할 경로
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
