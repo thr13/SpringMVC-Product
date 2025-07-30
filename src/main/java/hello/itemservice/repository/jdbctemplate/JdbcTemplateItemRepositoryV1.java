fba6dbb4c4ff0817564d78425e99573220a77c8e
thr13 <skhrnt2945@gmail.com>
2025-07-30 09:10:35 +0900

    fix: 잘못된 의존관계 주입 수정 JdbcTemplateItemRepositoryV1 가 외부에서 DataSource 를 주입받아 JdbcTemplate() 객체를 생성하도록 수정

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import javax.sql.DataSource;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +28,8 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 
     private final JdbcTemplate template;
 
-    public JdbcTemplateItemRepositoryV1(JdbcTemplate template) {
-        this.template = template;
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
+        this.template = new JdbcTemplate(dataSource);
     }
 
     @Override
